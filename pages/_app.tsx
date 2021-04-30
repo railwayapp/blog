603201a1e5a355a5abad5a0c8d84c408ef107923
Faraz Patankar <farazpatankar@gmail.com>
2021-05-01 01:14:14 +0400

    add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,11 @@ import '@styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-const RailwayBlog = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-)
+import useFathom from '@hooks/useFathom'
+
+const RailwayBlog = ({ Component, pageProps }: AppProps) => {
+  useFathom(process.env.NEXT_PUBLIC_FATHOM_CODE ?? '', 'blog.railway.app')
+  return <Component {...pageProps} />
+}
 
 export default RailwayBlog
