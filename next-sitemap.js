f8c9fdc7de64a4ab6fc9126df9450fc10c3bcfea
Faraz Patankar <farazpatankar@gmail.com>
2021-05-14 17:22:10 +0400

    add dynamic sitemap + robots.txt

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://blog.railway.app/',
+  generateRobotsTxt: true,
+}
---
+++ pages/sitemap.xml/index.tsx
@@ -0,0 +1,34 @@
+import { getServerSideSitemap } from 'next-sitemap'
+import { GetServerSideProps } from 'next'
+
+import getBlogIndex from '@lib/notion/getBlogIndex'
+import { getBlogLink } from '@lib/blog-helpers'
+
+const ROOT_URL = 'https://blog.railway.app'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const postsTable = await getBlogIndex()
+
+  const paths = Object.keys(postsTable)
+    .filter((post) => postsTable[post].Published === 'Yes')
+    .map((slug) => {
+      return {
+        loc: ROOT_URL + getBlogLink(slug),
+        lastmod: new Date().toISOString(),
+      }
+    })
+
+  const fields = [
+    {
+      loc: ROOT_URL,
+      lastmod: new Date().toISOString(),
+    },
+    ...paths,
+  ]
+
+  return getServerSideSitemap(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+const SitemapPage = () => {}
+export default SitemapPage
