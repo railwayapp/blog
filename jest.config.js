f622104e9ff8b8422277b4cc015741af0288179b
Faraz Patankar <farazpatankar@gmail.com>
2021-10-07 13:26:31 +0400

    update test

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,10 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '@components/(.*)': '<rootDir>/components/$1',
+    '@layouts/(.*)': '<rootDir>/layouts/$1',
+    '@lib/(.*)': '<rootDir>/lib/$1',
+    '@hooks/(.*)': '<rootDir>/hooks/$1',
+    '@styles/(.*)': '<rootDir>/styles/$1',
   },
 }
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+import { render } from '../testUtils'
+import Home from '../../pages/index'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
+    const { asFragment } = render(<Home posts={[]} preview={false} />, {})
     expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
   })
 })
