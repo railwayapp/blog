10a033bd2e509b768a8ba2ea3d05bbff7f5a40f2
Jake Runzer <jakerunzer@gmail.com>
2022-02-25 02:02:27 -0500

    Mobile improvments (#10)
    * better index page title on mobile
    
    * hide background on mobile
    
    * default theme is light

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,8 @@ const Home: NextPage<Props> = ({ posts = [] }) => {
   return (
     <Page>
       <div className="max-w-5xl px-4 mx-auto">
-        <header className="py-24">
-          <h1 className="text-6xl font-bold text-center leading-10">
+        <header className="py-16 md:py-24">
+          <h1 className="text-5xl md:text-6xl leading-tight font-bold text-center">
             Railway Blog
           </h1>
         </header>
