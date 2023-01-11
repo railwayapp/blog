2756cc8c1aaff76cb06c265d764fd87ad085f7a2
Jake Runzer <jakerunzer@gmail.com>
2023-01-10 20:45:38 -0500

    slight background

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -5,20 +5,31 @@ import PostItem from "./PostItem"
 
 export const PostList: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
   return (
-    <div className="max-w-6xl px-5 md:px-8 mx-auto">
-      <Categories />
+    <>
+      <div className="max-w-6xl px-5 md:px-8 mx-auto">
+        <Categories />
 
-      {posts.length === 0 ? (
-        <div className="text-center text-gray-500">Pretty empty here</div>
-      ) : (
-        <div className="posts max-w-5xl">
-          {posts
-            .filter((p) => p.properties.Published.checkbox)
-            .map((p) => (
-              <PostItem key={p.id} post={p} />
-            ))}
-        </div>
-      )}
-    </div>
+        {posts.length === 0 ? (
+          <div className="text-center text-gray-500">Pretty empty here</div>
+        ) : (
+          <div className="posts max-w-5xl">
+            {posts
+              .filter((p) => p.properties.Published.checkbox)
+              .map((p) => (
+                <PostItem key={p.id} post={p} />
+              ))}
+          </div>
+        )}
+      </div>
+
+      <img
+        src="/grid.svg"
+        className="absolute top-24 left-0 transform scale-x-[-1] max-w-none opacity-40"
+      />
+      <img
+        src="/grid.svg"
+        className="absolute top-24 right-0 max-w-none opacity-40"
+      />
+    </>
   )
 }
