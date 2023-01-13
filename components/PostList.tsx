bec0f80f1cbf79eca38d57a964eea268d808f5b6
Jake Runzer <jakerunzer@gmail.com>
2023-01-13 18:01:44 -0500

    load more button

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { PostProps } from "../lib/types"
 import { Categories } from "./Categories"
 import { FeaturedPostItem } from "./FeaturedPostItem"
 import PostItem from "./PostItem"
+
+const DEFAULT_POSTS_LENGTH = 12
 
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 
@@ -12,6 +14,8 @@ export const PostList: React.FC<{ posts: PostProps[]; category?: string }> = ({
 }) => {
   const featuredPosts = posts.filter((p) => p.properties.Featured.checkbox)
   const otherPosts = posts.filter((p) => !p.properties.Featured.checkbox)
+
+  const [showMore, setShowMore] = useState(false)
 
   return (
     <>
@@ -37,12 +41,24 @@ export const PostList: React.FC<{ posts: PostProps[]; category?: string }> = ({
               {capitalize(category ?? "Everything")}
             </h2>
 
-            <div className="col-span-1 lg:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-8 [&>*:nth-last-child(1)]:border-transparent md:[&>*:nth-last-child(2)]:border-transparent">
+            <div className="col-span-1 lg:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-8 [&>*:nth-last-child(2)]:border-transparent md:[&>*:nth-last-child(3)]:border-transparent">
               {otherPosts
                 .filter((p) => p.properties.Published.checkbox)
+                .slice(0, showMore ? undefined : DEFAULT_POSTS_LENGTH)
                 .map((p) => (
                   <PostItem key={p.id} post={p} />
                 ))}
+
+              {showMore ? (
+                <div />
+              ) : (
+                <button
+                  className="col-span-2 w-full text-center border border-pink-200 rounded-md px-4 py-2"
+                  onClick={() => setShowMore(true)}
+                >
+                  Load more posts...
+                </button>
+              )}
             </div>
           </div>
         )}
