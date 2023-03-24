aaf9b16b66d2bd13b8563230d8c16eefce143611
David <dbanys@users.noreply.github.com>
2023-03-24 12:50:34 -0700

    YAML syntax highlighting (#33)
    * YAML syntax highlighting
    
    * Ruby syntax highlighting

--- components/Code.tsx
+++ components/Code.tsx
@@ -20,7 +20,7 @@ export const Code: React.FC<{ children: string; language?: string }> = ({
   useAsyncEffect(async () => {
     setMarkupToHighlight(
       await getHighlighter({
-        langs: ["html", "javascript", "typescript", "shell", "rust", "go"],
+        langs: ["html", "javascript", "typescript", "shell", "rust", "go", "yaml", "ruby"],
         theme: theme === "light" ? LIGHT_THEME : DARK_THEME,
       }).then((highlighter) =>
         highlighter.codeToHtml(children, { lang: language })
