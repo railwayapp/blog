f7332a1993a93fcea8fae3db5d158b2f8c01a229
Faraz Patankar <farazpatankar@gmail.com>
2023-11-15 21:10:20 +0400

    add notion image url (#42)

--- next.config.js
+++ next.config.js
@@ -6,8 +6,11 @@ module.exports = {
     domains: [
       "user-images.githubusercontent.com",
       "og.railway.app",
-      "s3.us-west-2.amazonaws.com",
       "res.cloudinary.com",
+
+      // Images from Notion
+      "s3.us-west-2.amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
   },
 }
