d05a275ebf6e62fc2d1242397e895f8c577c58c3
Aleks Rūtiņš <aleks@rutins.com>
2024-12-02 23:04:49 -0500

    Python syntax highlighting (#56)

--- components/Code.tsx
+++ components/Code.tsx
@@ -20,7 +20,7 @@ export const Code: React.FC<{ children: string; language?: string }> = ({
   useAsyncEffect(async () => {
     setMarkupToHighlight(
       await getHighlighter({
-        langs: ["html", "javascript", "typescript", "shell", "rust", "go", "yaml", "ruby"],
+        langs: ["html", "javascript", "typescript", "shell", "rust", "go", "yaml", "ruby", "python"],
         theme: theme === "light" ? LIGHT_THEME : DARK_THEME,
       }).then((highlighter) =>
         highlighter.codeToHtml(children, { lang: language })
