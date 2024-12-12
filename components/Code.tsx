e54049b810d495b99110f5a1bd253d8237b51afb
Melissa Hale <melissakayh@gmail.com>
2024-12-12 12:34:07 -0600

    update shiki and migrate to new api
    Signed-off-by: Melissa Hale <melissakayh@gmail.com>

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,9 +1,8 @@
 import { useTheme } from "next-themes"
 import React, { useState } from "react"
-import { getHighlighter, setCDN } from "shiki"
+import { createHighlighter } from "shiki"
 import { useAsyncEffect } from "../hooks/useAsyncEffect"
 
-setCDN("https://unpkg.com/shiki/")
 
 const LIGHT_THEME = "github-light"
 const DARK_THEME = "github-dark-dimmed"
@@ -19,14 +18,15 @@ export const Code: React.FC<{ children: string; language?: string }> = ({
 
   useAsyncEffect(async () => {
     setMarkupToHighlight(
-      await getHighlighter({
-        langs: ["html", "javascript", "typescript", "shell", "rust", "go", "yaml", "ruby", "python"],
-        theme: theme === "light" ? LIGHT_THEME : DARK_THEME,
+      await createHighlighter({
+        langs: ["html", "javascript", "typescript", "shell", "rust", "go", "yaml", "ruby", "python", "graphql"],
+        themes: [theme === "light" ? LIGHT_THEME : DARK_THEME],
       }).then((highlighter) =>
-        highlighter.codeToHtml(children, { lang: language })
+        highlighter.codeToHtml(children, { lang: language, theme: theme === "light" ? LIGHT_THEME : DARK_THEME })
       )
     )
   }, [theme])
+  
 
   // Show raw code if highlighting hasn't completed yet
   if (markupToHighlight == null) {
