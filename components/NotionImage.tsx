5a189aa588dfcbd9df81640036dd159b21f81242
Mahmoud Abdelwahab <m@mahmoudw.com>
2025-10-22 01:04:33 +0300

    Minor styling fixes

--- components/NotionImage.tsx
+++ components/NotionImage.tsx
@@ -1,31 +1,31 @@
-import NextImage from "next/image"
-import { useState } from "react"
+import NextImage from "next/image";
+import { useState } from "react";
 
 export const NotionImage: React.FC<{
-  src: string
-  alt: string
-  blockId: string
+	src: string;
+	alt: string;
+	blockId: string;
 }> = ({ src, alt, blockId }) => {
-  const [imageSrc, setImageSrc] = useState(src)
+	const [imageSrc, setImageSrc] = useState(src);
 
-  return (
-    <div className="imageContainer w-full">
-      <NextImage
-        src={imageSrc}
-        alt={alt}
-        fill
-        // layout="fill"
-        // objectFit="cover"
-        // objectPosition="center"
-        className="nextImage p-0 rounded overflow-hidden"
-        unoptimized={process.env.NODE_ENV !== "production"}
-        onError={async () => {
-          const res = await fetch(`/api/image?blockId=${blockId}`).then((res) =>
-            res.json()
-          )
-          setImageSrc(res.imageSrc)
-        }}
-      />
-    </div>
-  )
-}
+	return (
+		<div className="imageContainer shadow-sm w-full">
+			<NextImage
+				src={imageSrc}
+				alt={alt}
+				fill
+				// layout="fill"
+				// objectFit="cover"
+				// objectPosition="center"
+				className="nextImage p-0 rounded overflow-hidden"
+				unoptimized={process.env.NODE_ENV !== "production"}
+				onError={async () => {
+					const res = await fetch(`/api/image?blockId=${blockId}`).then((res) =>
+						res.json(),
+					);
+					setImageSrc(res.imageSrc);
+				}}
+			/>
+		</div>
+	);
+};
--- components/NotionTable.tsx
+++ components/NotionTable.tsx
@@ -1,31 +1,31 @@
-import { Block } from "@notionhq/client/build/src/api-types"
-import { NotionText, TextProps } from "./NotionText"
+import type { Block } from "@notionhq/client/build/src/api-types";
+import { NotionText, type TextProps } from "./NotionText";
 
 interface TableRow {
-  cells: TextProps[][]
+	cells: TextProps[][];
 }
 
 interface Table {
-  has_column_header: boolean
-  has_row_header: boolean
-  children: Block[]
+	has_column_header: boolean;
+	has_row_header: boolean;
+	children: Block[];
 }
 
 const TableRow = ({ row }: { row: TableRow }) => {
-  return (
-    <tr>
-      {row.cells.map((c, idx) => {
-        return (
-          <td key={idx}>
-            {c.map((c, idx) => {
-              return <NotionText key={idx} text={[c]} />
-            })}
-          </td>
-        )
-      })}
-    </tr>
-  )
-}
+	return (
+		<tr>
+			{row.cells.map((c, idx) => {
+				return (
+					<td key={idx} className="px-4 py-3 text-sm text-gray-800">
+						{c.map((c, idx) => {
+							return <NotionText key={idx} text={[c]} />;
+						})}
+					</td>
+				);
+			})}
+		</tr>
+	);
+};
 
 /**
  * Barebones table component that only supports tables with:
@@ -33,29 +33,29 @@ const TableRow = ({ row }: { row: TableRow }) => {
  * - text cells
  */
 export const NotionTable = ({ table }: { table: Table }) => {
-  const baseRows = table.children
-    // @ts-ignore: Current client version does not support `table_row` but API does
-    .filter((c) => c.type === "table_row")
-    // @ts-ignore: Current client version does not support `table_row` but API does
-    .map((c) => c.table_row)
+	const baseRows = table.children
+		// @ts-ignore: Current client version does not support `table_row` but API does
+		.filter((c) => c.type === "table_row")
+		// @ts-ignore: Current client version does not support `table_row` but API does
+		.map((c) => c.table_row);
 
-  const columnHeaders = table.has_column_header ? baseRows[0] : null
-  const rows = table.has_column_header ? baseRows.slice(1) : baseRows
+	const columnHeaders = table.has_column_header ? baseRows[0] : null;
+	const rows = table.has_column_header ? baseRows.slice(1) : baseRows;
 
-  return (
-    <div className="overflow-x-auto">
-      <table className="prose">
-        {columnHeaders != null && (
-          <thead className="font-medium">
-            <TableRow row={columnHeaders} />
-          </thead>
-        )}
-        <tbody>
-          {rows.map((r) => (
-            <TableRow key={r.id} row={r} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+	return (
+		<div className="overflow-x-auto my-8 -mx-5 md:-mx-0">
+			<table className="w-full min-w-full border-collapse">
+				{columnHeaders != null && (
+					<thead className="font-medium bg-gray-50 border-b border-gray-200">
+						<TableRow row={columnHeaders} />
+					</thead>
+				)}
+				<tbody className="divide-y divide-gray-200">
+					{rows.map((r) => (
+						<TableRow key={r.id} row={r} />
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
