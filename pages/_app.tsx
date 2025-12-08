30ed173468bb438ffee274ad3bfce68ce4b60ac1
Paulo Cabral Sanz <paulo@railway.app>
2025-12-08 15:19:58 -0300

    Upgrade Next and React
    Railway http proxy protects us, but let's update it.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import "@styles/globals.css"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
 import Head from "next/head"
-import { Inter } from "@next/font/google"
+import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
 
